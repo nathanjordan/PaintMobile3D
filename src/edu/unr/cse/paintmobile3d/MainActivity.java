3b73330b7cacefb39f5b2c3aa6d8ba5a245af74f
Cagri Ates <hcagri@gmail.com>
2012-12-14 05:37:18 -0800

    Settings page added

--- src/edu/unr/cse/paintmobile3d/MainActivity.java
+++ src/edu/unr/cse/paintmobile3d/MainActivity.java
@@ -1,9 +1,10 @@
 package edu.unr.cse.paintmobile3d;
 
-import android.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -16,8 +17,28 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
         return true;
     }
+    
+	  @Override
+	  public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.item1:
+	    	//Pop-up menu for save, load, clear, share links
+	      break;
+	    case R.id.item2:
+	        //Color picker
+	      break;
+	    case R.id.item3:
+	      startActivity(new Intent(this, SettingsActivity.class));
+	      break;
+
+	    default:
+	      break;
+	    }
+
+	    return true;
+	  }
     
 }
